8731c2650d5f09ebc6e67d244de8d538d05830bf
Kee Yeong Tan <keeyeong.tan@gmail.com>
2017-02-06 23:39:40 +0100

    Parallel Counting Change

--- reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -14,7 +14,7 @@ object ParallelCountChangeRunner {
     Key.exec.maxWarmupRuns -> 40,
     Key.exec.benchRuns -> 80,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val amount = 250
@@ -43,32 +43,57 @@ object ParallelCountChangeRunner {
 object ParallelCountChange {
 
   /** Returns the number of ways change can be made from the specified list of
-   *  coins for the specified amount of money.
-   */
+    * coins for the specified amount of money.
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    ???
+    def countChangeIter(money: Int, coins: List[Int]): Int = {
+      if (coins.isEmpty || money < 0) 0
+      else if (money == coins.head) 1
+      else {
+        val (a, b) = (countChangeIter(money - coins.head, coins), countChangeIter(money, coins.tail))
+        a + b
+      }
+    }
+
+    if (money == 0) 1
+    else
+      countChangeIter(money, coins)
   }
 
   type Threshold = (Int, List[Int]) => Boolean
 
   /** In parallel, counts the number of ways change can be made from the
-   *  specified list of coins for the specified amount of money.
-   */
+    * specified list of coins for the specified amount of money.
+    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    ???
+    def parCountChangeIter(money: Int, coins: List[Int]): Int = {
+      if (coins.isEmpty || money < 0) 0
+      else if (money == coins.head) 1
+      else {
+        if (threshold(money,coins)) countChange(money, coins)
+        else {
+          val (a, b) = parallel(parCountChangeIter(money - coins.head, coins), parCountChangeIter(money, coins.tail))
+          a + b
+        }
+      }
+    }
+
+    if (money == 0) 1
+    else
+      parCountChangeIter(money, coins)
   }
 
   /** Threshold heuristic based on the starting money. */
   def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+    (amountLeft: Int, coinsLeft: List[Int]) => amountLeft <= 2 * startingMoney / 3
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
+    (amountLeft: Int, coinsLeft: List[Int]) => coinsLeft.size <= 2 * totalCoins / 3
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
   def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ???
+    (amountLeft: Int, coinsLeft: List[Int]) => coinsLeft.size * amountLeft <= 2 * allCoins.size * startingMoney / 3
   }
 }
--- reductions/src/test/scala/reductions/ParallelCountChangeSuite.scala
+++ reductions/src/test/scala/reductions/ParallelCountChangeSuite.scala
@@ -13,7 +13,7 @@ import ParallelCountChange._
 class ParallelCountChangeSuite extends FunSuite {
 
   test("countChange should return 0 for money < 0") {
-    def check(money: Int, coins: List[Int]) = 
+    def check(money: Int, coins: List[Int]) =
       assert(countChange(money, coins) == 0,
         s"countChang($money, _) should be 0")
 
@@ -24,7 +24,7 @@ class ParallelCountChangeSuite extends FunSuite {
   }
 
   test("countChange should return 1 when money == 0") {
-    def check(coins: List[Int]) = 
+    def check(coins: List[Int]) =
       assert(countChange(0, coins) == 1,
         s"countChang(0, _) should be 1")
 
@@ -34,7 +34,7 @@ class ParallelCountChangeSuite extends FunSuite {
   }
 
   test("countChange should return 0 for money > 0 and coins = List()") {
-    def check(money: Int) = 
+    def check(money: Int) =
       assert(countChange(money, List()) == 0,
         s"countChang($money, List()) should be 0")
 
@@ -63,5 +63,62 @@ class ParallelCountChangeSuite extends FunSuite {
     check(250, List(1, 2, 5, 10, 20, 50), 177863)
   }
 
+  test("parCountChange should return 0 for money < 0") {
+    def check(money: Int, coins: List[Int]) =
+      assert(parCountChange(money, coins, (_, _) => false) == 0,
+        s"parCountChang($money, _) should be 0")
 
+    check(-1, List())
+    check(-1, List(1, 2, 3))
+    check(-Int.MinValue, List())
+    check(-Int.MinValue, List(1, 2, 3))
+  }
+
+  test("parCountChange should return 1 when money == 0") {
+    def check(coins: List[Int]) =
+      assert(parCountChange(0, coins, (_, _) => false) == 1,
+        s"parCountChang(0, _) should be 1")
+
+    check(List())
+    check(List(1, 2, 3))
+    check(List.range(1, 100))
+  }
+
+  test("parCountChange should return 0 for money > 0 and coins = List()") {
+    def check(money: Int) =
+      assert(parCountChange(money, List(), (_, _) => false) == 0,
+        s"parCountChang($money, List()) should be 0")
+
+    check(1)
+    check(Int.MaxValue)
+  }
+
+  test("parCountChange should work when there is only one coin") {
+    def check(money: Int, coins: List[Int], expected: Int) =
+      assert(parCountChange(money, coins, (_, _) => false) == expected,
+        s"parCountChange($money, $coins) should be $expected")
+
+    check(1, List(1), 1)
+    check(2, List(1), 1)
+    check(1, List(2), 0)
+    check(Int.MaxValue, List(Int.MaxValue), 1)
+    check(Int.MaxValue - 1, List(Int.MaxValue), 0)
+  }
+
+  test("parCountChange should work for example") {
+    def check(money: Int, coins: List[Int], expected: Int) =
+      assert(parCountChange(money, coins, (_, _) => false) == expected,
+        s"parCountChange($money, $coins) should be $expected")
+
+    check(4, List(1, 2), 3)
+  }
+
+  test("parCountChange should work for multi-coins") {
+    def check(money: Int, coins: List[Int], expected: Int) =
+      assert(parCountChange(money, coins, (_, _) => false) == expected,
+        s"parCountChange($money, $coins) should be $expected")
+
+    check(50, List(1, 2, 5, 10), 341)
+    check(250, List(1, 2, 5, 10, 20, 50), 177863)
+  }
 }

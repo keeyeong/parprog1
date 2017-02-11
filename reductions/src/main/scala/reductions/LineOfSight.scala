214727d591c89e11984fc952f5a508cf5d827bec
Kee Yeong Tan <keeyeong.tan@gmail.com>
2017-02-11 14:33:50 +0100

    first submission

--- reductions/src/main/scala/reductions/LineOfSight.scala
+++ reductions/src/main/scala/reductions/LineOfSight.scala
@@ -4,13 +4,13 @@ import org.scalameter._
 import common._
 
 object LineOfSightRunner {
-  
+
   val standardConfig = config(
     Key.exec.minWarmupRuns -> 40,
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 100,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]) {
     val length = 10000000
@@ -34,7 +34,22 @@ object LineOfSight {
   def max(a: Float, b: Float): Float = if (a > b) a else b
 
   def lineOfSight(input: Array[Float], output: Array[Float]): Unit = {
-    ???
+    var max = 0f
+    input.zipWithIndex.foreach {
+      (x: (Float, Int)) => {
+        if (x._2 == 0) {
+          output(0) = 0
+        } else {
+          val newVal = x._1 / x._2
+          if (newVal > max) {
+            output(x._2) = newVal
+            max = newVal
+          } else {
+            output(x._2) = max
+          }
+        }
+      }
+    }
   }
 
   sealed abstract class Tree {
@@ -48,45 +63,70 @@ object LineOfSight {
   case class Leaf(from: Int, until: Int, maxPrevious: Float) extends Tree
 
   /** Traverses the specified part of the array and returns the maximum angle.
-   */
+    */
   def upsweepSequential(input: Array[Float], from: Int, until: Int): Float = {
-    ???
+    input.slice(from, until).zipWithIndex.foldLeft(0f)((max: Float, x: (Float, Int)) => {
+      val distance = from + x._2
+      if (distance == 0) max
+      else
+        Math.max(max, x._1 / distance)
+    })
   }
 
   /** Traverses the part of the array starting at `from` and until `end`, and
-   *  returns the reduction tree for that part of the array.
-   *
-   *  The reduction tree is a `Leaf` if the length of the specified part of the
-   *  array is smaller or equal to `threshold`, and a `Node` otherwise.
-   *  If the specified part of the array is longer than `threshold`, then the
-   *  work is divided and done recursively in parallel.
-   */
+    * returns the reduction tree for that part of the array.
+    *
+    * The reduction tree is a `Leaf` if the length of the specified part of the
+    * array is smaller or equal to `threshold`, and a `Node` otherwise.
+    * If the specified part of the array is longer than `threshold`, then the
+    * work is divided and done recursively in parallel.
+    */
   def upsweep(input: Array[Float], from: Int, end: Int,
-    threshold: Int): Tree = {
-    ???
+              threshold: Int): Tree = {
+    val length = end - from
+    val mid = from + length / 2
+
+    if (length <= threshold) {
+      Leaf(from, end, upsweepSequential(input, from, end))
+    } else {
+      val (resLeft, resRight) = parallel(upsweep(input, from, mid, threshold), upsweep(input, mid, end, threshold))
+      Node(resLeft, resRight)
+    }
   }
 
   /** Traverses the part of the `input` array starting at `from` and until
-   *  `until`, and computes the maximum angle for each entry of the output array,
-   *  given the `startingAngle`.
-   */
+    * `until`, and computes the maximum angle for each entry of the output array,
+    * given the `startingAngle`.
+    */
   def downsweepSequential(input: Array[Float], output: Array[Float],
-    startingAngle: Float, from: Int, until: Int): Unit = {
-    ???
+                          startingAngle: Float, from: Int, until: Int): Unit = {
+    input.slice(from, until).zipWithIndex.foreach {
+      (x: (Float, Int)) => {
+        if (from == 0) output(from) = 0 else {
+          val distance = from + x._2
+          output(distance) = Math.max(upsweepSequential(input, from, distance + 1), startingAngle)
+        }
+      }
+    }
   }
 
   /** Pushes the maximum angle in the prefix of the array to each leaf of the
-   *  reduction `tree` in parallel, and then calls `downsweepTraverse` to write
-   *  the `output` angles.
-   */
+    * reduction `tree` in parallel, and then calls `downsweepTraverse` to write
+    * the `output` angles.
+    */
   def downsweep(input: Array[Float], output: Array[Float], startingAngle: Float,
-    tree: Tree): Unit = {
-    ???
+                tree: Tree): Unit = {
+    tree match {
+      case Leaf(from, until, prevMax) => downsweepSequential(input, output, startingAngle, from, until)
+      case y: Node => parallel(downsweep(input, output, y.maxPrevious, y.left), downsweep(input, output, y.maxPrevious, y.right))
+      case _ => throw new UnsupportedOperationException
+    }
   }
 
   /** Compute the line-of-sight in parallel. */
   def parLineOfSight(input: Array[Float], output: Array[Float],
-    threshold: Int): Unit = {
-    ???
+                     threshold: Int): Unit = {
+    val tree = upsweep(input, 0, input.length, threshold)
+    downsweep(input, output, 0, tree)
   }
 }
--- reductions/src/test/scala/reductions/LineOfSightSuite.scala
+++ reductions/src/test/scala/reductions/LineOfSightSuite.scala
@@ -8,9 +8,11 @@ import org.scalatest.junit.JUnitRunner
 import common._
 import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory
 
-@RunWith(classOf[JUnitRunner]) 
+@RunWith(classOf[JUnitRunner])
 class LineOfSightSuite extends FunSuite {
+
   import LineOfSight._
+
   test("lineOfSight should correctly handle an array of size 4") {
     val output = new Array[Float](4)
     lineOfSight(Array[Float](0f, 1f, 8f, 9f), output)
@@ -23,10 +25,20 @@ class LineOfSightSuite extends FunSuite {
     assert(res == 4f)
   }
 
+  test("upsweep should correctly handle the chunk 1 until 4 of an array of 4 elements") {
+    val res = upsweep(Array[Float](0f, 1f, 8f, 9f), 0, 4, 1)
+    println(res)
+  }
 
   test("downsweepSequential should correctly handle a 4 element array when the starting angle is zero") {
     val output = new Array[Float](4)
     downsweepSequential(Array[Float](0f, 1f, 8f, 9f), output, 0f, 1, 4)
+    assert(output.toList == List(0f, 1f, 4f, 4f))
+  }
+
+  test("parLineOfSight should correctly handle an array of size 4") {
+    val output = new Array[Float](4)
+    parLineOfSight(Array[Float](0f, 1f, 8f, 9f), output, 1)
     assert(output.toList == List(0f, 1f, 4f, 4f))
   }
 
